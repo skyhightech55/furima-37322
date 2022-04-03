a5ecc83c83a82130a8bd522dc29a65226d8d8a03
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-04-03 09:10:20 +0900

    バリデーション及びアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,17 @@ class Item < ApplicationRecord
   has_one :order
   belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :item_category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_day
 
-  
+  validates :name, :introduction, :item_category_id, :item_condition_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :price, :user, presence: true
+
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
