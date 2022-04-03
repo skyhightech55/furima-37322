ac5c12decc7c9adee753a8c6cf865efe702352df
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-04-03 09:57:11 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,14 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+  
   private
 
   def items_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   has_one :order
   belongs_to :user
   has_one_attached :image
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :item_category
-  belongs_to_active_hash :item_condition
-  belongs_to_active_hash :shipping_cost
-  belongs_to_active_hash :shipping_day
+  belongs_to :prefecture
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_day
 
   validates :name, :introduction, :item_category_id, :item_condition_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :price, :user, presence: true
 
