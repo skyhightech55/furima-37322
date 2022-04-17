975195ddd19b4c9dfa84e0fc1cf4b4bf54d3cb2d
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-04-17 12:52:30 +0900

    orderモデルの作成

--- db/migrate/20220417034001_create_deliver_addresses.rb
+++ db/migrate/20220417034001_create_deliver_addresses.rb
@@ -1,8 +1,14 @@
 class CreateDeliverAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :deliver_addresses do |t|
-
-      t.timestamps
+      t.string :post_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :address, null: false
+      t.string :building
+      t.string :telephone, null: false
+      t.references :order, null: false, foreign_key: true
+      t.timestamps 
     end
   end
 end
