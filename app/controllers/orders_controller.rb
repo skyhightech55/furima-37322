77716311f7949f5d3d28079c57aa566a977b8a6a
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-04-18 20:54:03 +0900

    items_controllerの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,15 @@
 class OrdersController < ApplicationController
   def index
+    @deliver_addres = deliverAddress.new
+  end
+
+  def create
+    @deliver_address = DeliverAddress.new(order_params)
+  end
+
+  private
+
+  def order_params
+    params.require(:deliver_address).permit(:post_code, :prefecture, :city, :address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
