dc2d5f44cd867df61afb902ed27b915f5ed0e2fb
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-04-30 11:45:57 +0900

    @item = Item.find(params[:item_id])の記述をまとめる実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
     if @item.user_id == current_user.id || @item.order != nil
       redirect_to root_path
@@ -9,7 +9,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
@@ -29,6 +28,10 @@ class OrdersController < ApplicationController
   
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   
