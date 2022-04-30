57a9ea757eaca324113dff10bb7ff4b743f8f2e9
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-04-30 10:46:37 +0900

    URL直接入力対策の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    if @item.user_id == current_user.id || @item.order != nil
+      redirect_to root_path
+    end
   end
 
   def create
@@ -22,7 +26,7 @@ class OrdersController < ApplicationController
   end
 
   private
-
+  
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
