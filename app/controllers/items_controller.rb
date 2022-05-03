d67c813060db05713090ebc28e918ce2631475e7
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-05-03 19:04:08 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,7 @@ class ItemsController < ApplicationController
 
   def edit
     redirect_to root_path unless current_user.id == @item.user_id
-    if @item.order != nil
-      redirect_to root_path
-    end
+    
   end
 
   def update
